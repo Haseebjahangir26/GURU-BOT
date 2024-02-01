3e437bf69aadf64d9b87161984932b1093bfa5c6
Haseebjahangir26 <146482424+Haseebjahangir26@users.noreply.github.com>
2024-02-01 20:15:39 +0500

    Update config.js

--- config.js
+++ config.js
@@ -8,7 +8,7 @@ import axios from 'axios'
 
 global.owner = [
   ['923346778315', 'Jutt Bot', true],
-  ['6281255369012', 'Ash', true], 
+  ['923346778315', 'Jahangir', true], 
   [''] 
 ] //Number of owners
 
@@ -50,9 +50,9 @@ global.APIKeys = { // APIKey Here
 }
 
 // Sticker WM
-global.botname = 'ᴛʜᴇ ɢᴜʀᴜ-ʙᴏᴛ'
-global.premium = 'true'
-global.packname = 'GURU┃ᴮᴼᵀ' 
+global.botname = 'Jutt Bot'
+global.premium = 'false'
+global.packname = 'Jahangir' 
 global.author = '@Asliguru' 
 global.menuvid = 'https://telegra.ph/file/085c4b1068f0f4f8db970.mp4'
 global.igfg = '▢ Follow on Instagram\nhttps://www.instagram.com/asli_guru69\n' 
